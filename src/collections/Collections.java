45f5be5107609e0dc23973891c806b2fc1de94a6
Sander2022164 <2022164@student.cct.ie>
2023-10-18 13:31:41 +0100

    Rectangle class created
    Rectangle class was created and also an array with 2 rectangles

--- src/collections/Collections.java
+++ src/collections/Collections.java
@@ -15,6 +15,9 @@ public class Collections {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Rectangle[] rectangles= {new Rectangle(2,3), new Rectangle(4,1)};
+        
     }
     
 }
